4c2c59e2dda3ff098a5af7a0676293c31ab8acb4
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-08-15 11:12:13 +1000

    Commented a code plan to follow using the Readme

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   //
+  // Code Plan out
+  //  I need a var to store the the user input
+  //  I will need to prompt the user to input a password length
+  //  I will need to check if that password lenght is within 8-128
+  //  Then I need to check if that the user input is a Number
+  // Then I will need to ask if they want special case letters, then numbers, then uppercase.
+  // Need to write if else statements to track the users selection
+  // then will need to take all the user selected answer and genterate a randow string
+  //
 }
 
 // Write password to the #password input
@@ -15,7 +24,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-if (generateBtn) {
-  add;
-}
